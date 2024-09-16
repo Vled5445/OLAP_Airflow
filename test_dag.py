342f7236cc471471e92db7a82bc6b9ba9a3fd00b
Vled5445 <101985341+Vled5445@users.noreply.github.com>
2024-09-16 12:02:07 +0300

    Update test_dag.py

--- test_dag.py
+++ test_dag.py
@@ -3,7 +3,7 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime,timedelta
 from clickhouse_driver import Client
 from sqlalchemy import create_engine
-default_args = {'owner':'vlados','start_date': datetime(2024, 7, 22)}
+default_args = {'owner':'vlad','start_date': datetime(2024, 7, 22)}
 
 
 dag = DAG(dag_id='test_dag',
@@ -25,11 +25,11 @@ def main():
     client.execute("""insert into report.gi_agg as select toDate(dt) dt_hour ,
     employee_id ,
     office_id ,
-    wh_id ,
+    wh ,
     uniq(gi) ,
-    status_id
+    stat
     from gi 
-    group by dt_hour,employee_id,office_id,wh_id,status_id""")
+    group by dt_hour,employee_id,office_id,wh,stat""")
     df = client.query_dataframe('select * from report.gi_agg')
     engine = create_engine('postgresql://default:5445@some-postgres:5432/report')
     df.to_sql('report.gi_agg',engine,if_exists='append')
